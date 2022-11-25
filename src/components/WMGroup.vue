d9e762af485856d16d64371c6891fdb902ce9c3d
saidov-arb <72748255+saidov-arb@users.noreply.github.com>
2022-11-25 22:52:48 +0100

    2 files modified, 3 added

---
+++ src/components/WMGroup.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="wmgroup">
+    <h3>Group {{ groupchar }}</h3>
+  </div>
+</template>
+
+<script>
+import { ref, reactive, toRefs } from 'vue';
+
+export default {
+  props: {
+    groupchar: {
+      type: String,
+      required: true,
+    },
+    members: {
+      type: Array,
+      required: true,
+    },
+  },
+  setup(props) {
+    return {};
+  },
+};
+</script>
+
+<style scoped></style>
--- src/components/WMList.vue
+++ src/components/WMList.vue
@@ -7,19 +7,20 @@
       <div v-if="loading">
         <h2>Lodaing Data...</h2>
       </div>
-      <div v-else>
+      <div v-else-if="data">
         <h1>WM</h1>
-        <h2>Countries</h2>
+        <!-- <h2>Countries</h2>
         <ul>
-          <li v-if="data" v-for="item in data.flags">
+          <li v-for="item in data.flags">
             <img :src="item.link" />
             {{ item.country }}
           </li>
-        </ul>
+        </ul> -->
         <h2>Groups</h2>
         <ul>
-          <li v-if="data" v-for="item in data.groups">
-            {{ item.group }}
+          <li v-for="item in data.groups">
+            <!-- {{ item.group }} -->
+            <WMGroup :groupchar="item.group" :members="item.members" />
           </li>
         </ul>
       </div>
@@ -28,10 +29,14 @@
 </template>
 
 <script>
-import { useFetch } from '../composable/UseFetch.js';
 import { ref, reactive, toRefs } from 'vue';
+import { useFetch } from '../composable/UseFetch.js';
+import WMGroup from './WMGroup.vue';
 
 export default {
+  components: {
+    WMGroup,
+  },
   setup() {
     const { data, error, loading } = useFetch(
       'https://raw.githubusercontent.com/htlWels/WM/main/spielplan.json',
@@ -55,5 +60,6 @@ h1 {
 img {
   width: 30px;
   height: 20px;
+  border-radius: 5px;
 }
 </style>
